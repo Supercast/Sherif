f5a913bc8abc0f29621525cf1a0ae7403c02d1fa
Beetlejuice <marius.40.lemonnier@gmail.com>
2017-12-05 14:16:45 +0100

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const token = "MzgzNTY5OTk1MDUwOTA5Njk3.DPmwBg.2T3W08C6-rbbbc3kp4-wyNmo_MM";
+var prefix        = "s!";
+var mention       = "<@383569995050909697>";
+var sherif 		  = '<:sherif:258244124220588043>';
+
+client.on('ready', () => {
+	console.log("Bonjour, je suis LE Shérif.");
+    client.user.setGame("Far West");
+});
+
+client.on('message', message => {
+	var msgc = message.content;
+
+	if (!message.author.bot) {
+	    if (msgc.includes('ping') || msgc.includes('Ping') || msgc.includes('pong') || msgc.includes('Pong')) {
+	    	message.channel.send('On ne joue pas avec le Shérif ' + sherif);
+	    }
+		var badwords = ["JUL", "Jul", "jul", "<:jul:365030665630121985>"];
+		var i = 0;
+		while(i<badwords.length){
+			if(msgc.includes(badwords[i])){
+				message.delete(1000);
+			}
+		i++;
+		}
+	}
+});
+
+client.login(token);
