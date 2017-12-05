359a86fb166880dfa823c66aad5a072ca616f43c
Beetlejuice <marius.40.lemonnier@gmail.com>
2017-12-05 17:18:13 +0100

    update

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ client.on('message', message => {
 	    	message.channel.send('On ne joue pas avec le Shérif ' + sherif);
 	    }else if (msgc.includes('Bonjour') || msgc.includes('bonjour')) {
         message.channel.send('Bien le bonjour ' + message.author.username + ', je suis le Shérif et je suis là pour faire régner l\'ordre.');
-			}
+			}else if (msgc.includes('BONJOUR')) {
+				message.channel.send('EH OH ! ' + message.author.username.toUpperCase() + ', TU VAS TE CALMER VITE FAIT ET ARRETER DE CRIER !');
 		var badwords = ["JUL", "Jul", "jul", "<:jul:365030665630121985>"];
 		var i = 0;
 		while(i<badwords.length){
